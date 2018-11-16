366f81c75dcaa1a0ce49e56b862c8df00540ad6c
luisgal <luisgalrod@gmail.com>
2018-11-16 00:56:47 -0600

    Se crea funcion javascript que escribe texto sobre html nos basaremos para modelar acticles y sections, con solo llamar a esta funcion y darle parametros de texto, imagenes que se agreguen, etc.

---
+++ funciones.js
@@ -0,0 +1,5 @@
+function saludar(){
+    document.write('<h1>Hola</h1><br>');
+    document.write('Con esto intentaremos funcion que escribe texto nos basaremos para modelar acticles y sections, con solo llamar a esta funcion y darle parametros de texto, imagenes que se agreguen');
+    document.write('<hr class="separador">');
+}
