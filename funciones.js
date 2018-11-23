fe5fc0a3bf4b338ff1e4a70984c5af60d50266b4
luisgal <luisgalrod@gmail.com>
2018-11-23 02:35:27 -0600

    Diseño base terminado, ajustes de footer completados, diseño agregado.
    Se añadio un mejor estilo a cada objeto añadiendo un fondo al main y 
    aside, modificando y añadiendo fondos de tables al igual que de footer. 
    Se añade fondo de 'PUG'.
    Plantilla casi lista. Falta modificar table que se despliega con el 
    modal del boton <VerMas>

--- funciones.js
+++ funciones.js
@@ -20,20 +20,46 @@ function articuloMediano(){
                 '<td>Videojuego basado en dispros que hacen pium pium y solo quiero rellenas informacion vale bye :v</td>',
             '</tr>',
             '<tr>',
-                '<td colspan="2"><a href="#openModal"><button><span>Ver Más</span></button></a></td>',
+                '<td colspan="2" class="boton"><a href="#openModal"><button><span>Ver Más</span></button></a></td>',
             '</tr>',
         '</table>',
         '<hr class="separadorH">',
         '<div id="openModal" class="modalDialog">',
             '<div>',
-                '<a href="#close" title="Close" class="close">X</a>',
-                '<h2>Mi modal</h2>',
-                '<p>Este es un ejemplo de modal, creado gracias al poder de CSS3.</p>',
-                '<p>Puedes hacer un montón de cosas aquí, como alertas o incluso crear un formulario de registro aquí mismo.</p>',
+                '<a href="#close" title="Close" class="close">X</a>');
+
+                tablePresentM();
+
+    document.write(
             '</div>',
         '</div>',
     '</article>'
     );
+}
+
+function tablePresentM(){
+    document.write(
+    '<article class="articulos">',
+        '<table class="descripcionCorta tablaMuestra">',
+            '<tr>',
+                '<td>Nombre</td>',
+                '<td>El Jeilo</td>',
+                '<td rowspan="5" class="sinFondo"><img src="styles/img/eljeilo.jpg" class="vistaMediana"></td>',
+            '</tr>',
+            '<tr>',
+                '<td>Precio</td>',
+                '<td>$5000</td>',
+            '</tr>',
+            '<tr>',
+                '<td>Calificacion</td>',
+                '<td>4.5 &#9733</td>',
+            '</tr>',
+            '<tr>',
+                '<td>Descripción</td>',
+                '<td>Videojuego basado en dispros que hacen pium pium y solo quiero rellenas informacion vale bye :v</td>',
+            '</tr>',
+        '</table>',
+    '</article>');
 }
 
 function tablePresent(){
